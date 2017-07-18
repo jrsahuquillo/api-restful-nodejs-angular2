bf3ec729b0373708a7ea98947a37b069f7adb5d8
jrsahuquillo <jrsahuquillo@gmail.com>
2017-07-19 00:37:39 +0200

    Mongoose set up

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 'use strict'
 
+const mongoose = require('mongoose')
 const app = require('./app')
 const port = process.env.PORT || 3678
 
-
-app.listen(port, function() {
-  console.log(`✅ ✅ ✅ Favorites API REST running on http://localhost:${port}`)
+mongoose.connect('mongodb://localhost:27017/favorites',(err, res) => {
+  if (err){
+    throw err;
+  }else{
+    console.log('✅ ✅ ✅ Successful connection to DB')
+    app.listen(port, function() {
+      console.log(`✅ ✅ ✅ Favorites API REST running on http://localhost:${port}`)
+    })
+  }
 })
