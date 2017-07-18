09fd1bf108ac18a96f9ac8429a7fbcee168bd6c8
jrsahuquillo <jrsahuquillo@gmail.com>
2017-07-19 00:04:31 +0200

    Controllers actions

--- controllers/favorite.js
+++ controllers/favorite.js
@@ -5,6 +5,35 @@ function test(req, res) {
   res.status(200).send({message: `Hello ${name}!`})
 }
 
+function getFavorite(req, res) {
+  let favoriteId = req.params.id
+  res.status(200).send({data: favoriteId})
+}
+
+function getFavorites(req, res) {
+
+}
+
+function saveFavorite(req, res) {
+  let params = req.body
+  res.status(200).send({favorite: params})
+}
+
+function updateFavorite(req, res) {
+  let params = req.body
+  res.status(200).send({update: true, favorite: params})
+}
+
+function deleteFavorite(req, res) {
+  let favoriteId = req.params.id
+  res.status(200).send({delete: true, data: favoriteId})
+}
+
 module.exports = {
-  test
+  test,
+  getFavorite,
+  getFavorites,
+  saveFavorite,
+  updateFavorite,
+  deleteFavorite
 }
--- routes/favorite.js
+++ routes/favorite.js
@@ -5,5 +5,9 @@ const favoriteController = require('../controllers/favorite')
 const api = express.Router()
 
 api.get('/prueba/:name?', favoriteController.test)
+api.get('/favorite/:id', favoriteController.getFavorite)
+api.post('/favorite', favoriteController.saveFavorite)
+api.put('/favorite', favoriteController.updateFavorite)
+api.delete('/favorite', favoriteController.deleteFavorite)
 
 module.exports = api
