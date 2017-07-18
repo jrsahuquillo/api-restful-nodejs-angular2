5984d0dd882e4c5dfa3b70c24526b91c4fbdb932
jrsahuquillo <jrsahuquillo@gmail.com>
2017-07-18 21:31:22 +0200

    Starting app, express installation and server settings

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+
+app.listen(3678, function() {
+  console.log('✅ ✅ ✅ Favorites API REST running on http://localhost:3678')
+});
